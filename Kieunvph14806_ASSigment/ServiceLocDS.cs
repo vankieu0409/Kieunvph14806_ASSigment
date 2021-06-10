64648ea215f9fd670623b38116f76b0ab1ca0639
Kiều Nguyễn <beu_csnv@outlook.com>
2021-06-10 23:46:07 +0700

    cái này mới

---
+++ Kieunvph14806_ASSigment/ServiceLocDS.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kieunvph14806_ASSigment
+{
+    partial class Service
+    {
+        public void LocDanhSach()
+        {
+            do
+            {
+                Console.Write(" 1. lọc theo nhà mạng.\n" +
+                              " 2. lọc theo tên\n" +
+                              " 3. lọc theo giới tính\n" +
+                              " 0. thoát" +
+                              " Mời bạn chọn CT: ");
+                _input = Console.ReadLine();
+                switch (_input)
+                {
+                    case "1":
+                        theoNHaMang();
+                        break;
+                    case "2":
+                        SapXepTheoten();
+                        break;
+                    case "3":
+                        SAPxeptheoGioiTinh();
+                        break;
+
+                    case "0":
+                        Console.WriteLine("");
+                        break;
+                    default:
+                        Console.WriteLine(" chương trình không có. MỜi chọn lại");
+                        break;
+
+                }
+                Console.WriteLine("                     *");
+            } while (!(_input == "0"));
+        }
+
+        public void theoNHaMang()
+        {
+            Console.WriteLine(" Những người dùng Việttel");
+            foreach (var x in _lstDanhBas.Where(c => c.Sdt1.StartsWith("098")).ToList())
+            {
+                x.InRaManHinh();
+                Console.WriteLine("----------");
+            }
+
+            foreach (var x in _lstDanhBas.Where(c => c.Ssdt2.StartsWith("098")).ToList())
+            {
+                x.InRaManHinh();
+                Console.WriteLine("----------");
+            }
+
+            Console.WriteLine(" \n" +
+                              "\n" +
+                              " Người dùng VInaphone");
+            foreach (var x in _lstDanhBas.Where(c => c.Sdt1.StartsWith("085")).ToList())
+            {
+                x.InRaManHinh();
+                Console.WriteLine("----------");
+            }
+
+            foreach (var x in _lstDanhBas.Where(c => c.Ssdt2.StartsWith("085")).ToList())
+            {
+                x.InRaManHinh();
+                Console.WriteLine("----------");
+            }
+
+        }
+
+        public void SapXepTheoten()
+        {
+
+            foreach (var x in _lstDanhBas.OrderBy(c => c.Ten).ToList())
+            {
+                x.InRaManHinh();
+                Console.WriteLine("----------");
+            }
+        }
+
+        public void SAPxeptheoGioiTinh()
+        {
+            Console.WriteLine(" Danh Bạ  liên lạc là Nam");
+            foreach (var x in _lstDanhBas.Where(c => c.GioiTinh == 1).ToList())
+            {
+                x.InRaManHinh();
+                Console.WriteLine("----------");
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine(" Danh Bạ  liên lạc là Nữ");
+            foreach (var x in _lstDanhBas.Where(c => c.GioiTinh == 1).ToList())
+            {
+                x.InRaManHinh();
+                Console.WriteLine("----------");
+            }
+        }
+    }
+}
